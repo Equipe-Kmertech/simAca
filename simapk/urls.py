66fd9a9575553527b184f42d9dcbf24a76eb4e67
nkega <njampouabnkegabrice@gmail.com>
2024-12-11 11:55:15 +0100

    index, about, details, courses

--- simapk/urls.py
+++ simapk/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path("courses", views.courses, name="courses"),
+    path("about", views.about, name="about"),
+    path("details", views.details, name="details"),
 ]
--- simapk/views.py
+++ simapk/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def courses(request):
+    return render(request, "courses.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def details(request):
+    return render(request, "details.html")
